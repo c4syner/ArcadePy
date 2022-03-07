785d78dfd6ed18f11d4dd2801637d66438309c51
KylerORourke <74610460+KylerORourke@users.noreply.github.com>
2022-03-07 04:01:03 -0600

    InputHandler

---
+++ TicTacToe/input_handler.py
@@ -0,0 +1,33 @@
+from operator import truediv
+from tkinter import Y
+
+
+class InputHandler:
+    def __init__(self) -> None:
+        pass
+    def _validateInput(self, board, x,y):
+        try:   
+            if(board[x][y] != 0):
+                return False
+            return True
+        except:
+            return False
+    
+    def getInput(self, board):
+        X_Coord = int(input("Pick an X coordiante "))
+        Y_Coord = int(input("Pick a Y coordinate "))
+        if(not self._validateInput(board, X_Coord, Y_Coord)):
+            self.getInput(board)
+        return (X_Coord, Y_Coord)
+
+
+
+            
+           
+           
+
+
+
+
+
+            
